64a92a2ce9ffea63cc11993775fcd7604327aa01
Siddhesh Deshmukh <140414567+SiddheshDeshmukh2212@users.noreply.github.com>
2024-04-21 02:01:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
